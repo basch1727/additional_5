86f8bdd483110057458e4b9514fd05b38447a58a
basch1727 <basch19900@gmail.com>
2018-09-27 15:06:29 +0300

    cheats

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let all = bracketsConfig.join(' ').replace(/,/g,'');
+  let arr = all.split(' ');
+  
+  let a = str.length;
+
+  while(a>0){
+    for(let i=0; i<arr.length; i++){
+      if(str.indexOf(arr[i])>=0){
+        str=str.replace(arr[i],'');
+        break;
+      }
+    }
+  a-=2;
+  }
+  if(str.length===0){
+    return true;
+  }
+  return false;
 }
